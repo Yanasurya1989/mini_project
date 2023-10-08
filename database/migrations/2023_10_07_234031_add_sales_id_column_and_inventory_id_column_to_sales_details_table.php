59ece23878508842fa1772ceef317d0f71ee9c8e
YanaWebProgrammer <teukita15@gmail.com>
2023-10-08 07:40:16 +0700

    foreign key with migrate

---
+++ database/migrations/2023_10_07_234031_add_sales_id_column_and_inventory_id_column_to_sales_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('sales_id')->required();
+            $table->foreign('sales_id')->references('id')->on('sales');
+
+            $table->unsignedBigInteger('inventory_id')->required();
+            $table->foreign('inventory_id')->references('id')->on('inventories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sales_details', function (Blueprint $table) {
+            $table->dropColumn('sales_id');
+            $table->dropColumn('inventory_id');
+        });
+    }
+};
